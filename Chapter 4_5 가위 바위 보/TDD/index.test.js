4c58978e9670aaa3acb72a3f425e2db3d1f0b497
choiseunghyeon <ket8780@gmail.com>
2021-06-05 00:07:08 +0900

    Chapter 4. 가위바위보 TDD 진행 중

---
+++ Chapter 4_5 가위 바위 보/TDD/index.test.js
@@ -0,0 +1,8 @@
+describe("Rock Scissor Paper", () => {
+  describe("Computer should return Rock Scissor Paper randomly", () => {
+    it("return Rock, Scissor or Paper string value with a one-third chance", () => {
+      const result = getOneOfRockScissorPaper();
+      expect(result).to;
+    });
+  });
+});
