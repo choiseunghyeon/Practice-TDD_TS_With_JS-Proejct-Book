53923be4cc881eea6296e1906bbd5ed38406d886
choiseunghyeon <ket8780@gmail.com>
2021-06-22 23:06:18 +0900

    Chapter 7. 농구 게임 만들기 리팩토링

--- Chapter 7. 농구 게임 만들기/Example/scripts.js
+++ Chapter 7. 농구 게임 만들기/Example/scripts.js
@@ -5,45 +5,40 @@ let shotsLeft = 15;
 
 function onComputerShoot() {
   if (!isCoumpterTurn) return;
-
-  const textElem = document.getElementById("text");
-  const comScoreElem = document.getElementById("computer-score");
   const shootType = Math.random() < 0.5 ? 2 : 3;
 
   if (shootType === 2) {
     if (Math.random() < 0.5) {
       // 1/2 확률
-      textElem.innerHTML = "컴퓨터가 2점 슛을 성공시켰습니다!";
+      showText("컴퓨터가 2점 슛을 성공시켰습니다!");
 
-      comScore += 2;
-      comScoreElem.innerHTML = comScore;
+      updateComputerScore(2);
     } else {
       //실패
-      textElem.innerHTML = "컴퓨터가 2점 슛을 실패했습니다.";
+      showText("컴퓨터가 2점 슛을 실패했습니다.");
     }
   } else {
     if (Math.random() < 0.33) {
       // 1/3 확률
-      comScore += 3;
-      comScoreElem.innerHTML = comScore;
-      textElem.innerHTML = "컴퓨터가 3점 슛을 성공시켰습니다!";
+      updateComputerScore(3);
+      showText("컴퓨터가 3점 슛을 성공시켰습니다!");
     } else {
       // 실패
-      textElem.innerHTML = "컴퓨터가 3점 슛을 실패했습니다.";
+      showText("컴퓨터가 3점 슛을 실패했습니다.");
     }
   }
 
-  disabledComputerButtons(true);
-  disabledUserButtons(false);
+  disableComputerButtons(true);
+  disableUserButtons(false);
 
   isCoumpterTurn = false;
 }
 
-function disabledComputerButtons(flag) {
+function disableComputerButtons(flag) {
   const computerButtons = document.getElementsByClassName("btn-computer");
   Array.from(computerButtons).forEach(btn => (btn.disabled = flag));
 }
-function disabledUserButtons(flag) {
+function disableUserButtons(flag) {
   const userButtons = document.getElementsByClassName("btn-user");
   Array.from(userButtons).forEach(btn => (btn.disabled = flag));
 }
@@ -51,34 +46,29 @@ function disabledUserButtons(flag) {
 function onUserShoot(shootType) {
   if (isCoumpterTurn) return;
 
-  const textElem = document.getElementById("text");
-  const userScoreElem = document.getElementById("user-score");
-
   if (shootType === 2) {
     if (Math.random() < 0.5) {
       // 1/2 확률
-      textElem.innerHTML = "2점 슛이 성공했습니다!";
+      showText("2점 슛이 성공했습니다!");
 
-      userScore += 2;
-      userScoreElem.innerHTML = userScore;
+      updateUserScore(2);
     } else {
       //실패
-      textElem.innerHTML = "2점 슛이 실패했습니다.";
+      showText("2점 슛이 실패했습니다.");
     }
   } else {
     if (Math.random() < 0.33) {
       // 1/3 확률
-      userScore += 3;
-      userScoreElem.innerHTML = userScore;
-      textElem.innerHTML = "3점 슛이 성공했습니다!";
+      updateUserScore(3);
+      showText("3점 슛이 성공했습니다!");
     } else {
       // 실패
-      textElem.innerHTML = "3점 슛이 실패했습니다.";
+      showText("3점 슛이 실패했습니다.");
     }
   }
 
-  disabledComputerButtons(false);
-  disabledUserButtons(true);
+  disableComputerButtons(false);
+  disableUserButtons(true);
   isCoumpterTurn = true;
 
   shotsLeft--;
@@ -87,14 +77,31 @@ function onUserShoot(shootType) {
 
   if (shotsLeft === 0) {
     if (userScore > comScore) {
-      textElem.innerHTML = "승리했습니다!";
+      showText("승리했습니다!");
     } else if (userScore < comScore) {
-      textElem.innerHTML = "아쉽게도 졌습니다....";
+      showText("아쉽게도 졌습니다....");
     } else {
-      textElem.innerHTML = "비겼습니다.";
+      showText("비겼습니다.");
     }
 
-    disabledComputerButtons(true);
-    disabledUserButtons(true);
+    disableComputerButtons(true);
+    disableUserButtons(true);
   }
+}
+
+function showText(message) {
+  const textElem = document.getElementById("text");
+  textElem.innerHTML = message;
+}
+
+function updateComputerScore(score) {
+  const comScoreElem = document.getElementById("computer-score");
+  comScore += score;
+  comScoreElem.innerHTML = comScore;
+}
+
+function updateUserScore(score) {
+  const userScoreElem = document.getElementById("user-score");
+  userScore += score;
+  userScoreElem.innerHTML = userScore;
 }
